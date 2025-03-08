8fae7f844e664b5013a048e716ca63e2a03af2d6
WIther <120669134+catgirlichinose@users.noreply.github.com>
2025-03-08 09:40:50 +0100

    Add installer and zsh config

---
+++ install.sh
@@ -0,0 +1,26 @@
+BLUE='\033[34m'
+NOTBLUE='\033[0m'
+
+printf ${BLUE}'\nInstalling YAY\n\n'${NOTBLUE}
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+printf ${BLUE}'\nInstalling necessary packages\n\n'${NOTBLUE}
+sudo pacman -Sy hyprland hyprpaper eza waybar rofi nautilus kitty fastfetch zoxide fzf firefox 
+yay -S oh-my-posh otf-codenewroman-nerd vesktop
+
+printf ${BLUE}'\nInstalling dotfiles\n\n'${NOTBLUE}
+
+mv config .config
+mv local .local
+mv mozilla .mozilla
+mv wallpaper .wallpaper 
+mv zshrc .zshrc
+
+rm -rf ~/.config/
+rm -rf ~/.local/
+rm -rf ~/.zshrc
+cd ~/dotfiles
+mv .* ~
