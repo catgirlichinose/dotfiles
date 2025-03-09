de9f6701680da74bd8f490a24fff401075a84cf9
WIther <120669134+catgirlichinose@users.noreply.github.com>
2025-03-09 17:31:41 +0100

    Update install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -7,9 +7,7 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-printf ${BLUE}'\nInstalling necessary packages\n\n'${NOTBLUE}
+printf ${BLUE}'\nInstalling packages\n\n'${NOTBLUE}
+
 sudo pacman -Sy hyprland hyprpaper eza waybar rofi nautilus kitty fastfetch zoxide fzf firefox 
 yay -S oh-my-posh otf-codenewroman-nerd vesktop
-
-printf ${BLUE}'\nInstalling dotfiles\n\n'${NOTBLUE}
-
