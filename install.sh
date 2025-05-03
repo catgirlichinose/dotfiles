13d6c2255c70549ec8566a1f12d5929c0fc36451
WIther <120669134+catgirlichinose@users.noreply.github.com>
2025-05-03 11:35:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# Check if git and/or networkmanager
+
+# Check if Git is installed
+if pacman -Qi git > /dev/null 2>&1; then
+    # Git is installed
+    is_git_installed=1
+else
+    # Git is NOT installed
+    is_git_installed=0
+fi
+
+# Check if NetworkManager is installed
+if pacman -Qi networkmanager > /dev/null 2>&1; then
+    if systemctl is-active --quiet NetworkManager; then
+        # NetworkManager is installed and running
+        is_nm_installed=1
+    else
+        # NetworkManager is installed but not running
+        is_nm_installed=2
+    fi
+else
+    # NetworkManager is NOT installed
+    is_nm_installed=0
+fi
+    
 # Define color variables for output
 RED='\e[31m'
 GREEN='\e[32m'
@@ -7,32 +32,50 @@ BLUE='\e[34m'
 BOLD='\e[1m'
 RESET='\e[0m'
 
-epic_print_function_green() {
-    echo -e "\n${BOLD}${GREEN}==>${RESET} ${BOLD}$1${RESET}"
+epic_print_function() {
+    case $2 in
+        green)
+            echo -e "\n${BOLD}${GREEN}==>${RESET} ${BOLD}$1${RESET}";;
+        blue)
+            echo -e "\n${BOLD}${BLUE}==>${RESET} ${BOLD}$1${RESET}";;
+        red)
+            echo -e "\n${BOLD}${RED}==>${RESET} ${BOLD}$1${RESET}";;
+    esac
 }
 
-epic_print_function_blue() {
-    echo -e "\n${BOLD}${BLUE}==>${RESET} ${BOLD}$1${RESET}"
+install_paru() {
+    if pacman -Qi paru > /dev/null 2>&1; then
+        epic_print_function "paru already installed, skipping." "blue"
+    else
+        # Install paru from AUR
+        epic_print_function "Installing paru
+        " "green"
+        git clone https://aur.archlinux.org/paru.git
+        pushd paru || { echo "Failed to enter 'paru' directory"; exit 1; }
+        makepkg -si --noconfirm
+        popd
+        rm -r paru/
+    fi
 }
 
-epic_print_function_red() {
-    echo -e "\n${BOLD}${RED}==>${RESET} ${BOLD}$1${RESET}"
-}
+case $is_git_installed in
+    1)
+        :;;
+    0)
+        sudo pacman -S --noconfirm git;;
+esac
 
-# Function to print messages in blue
+case $is_nm_installed in
+    0)
+        sudo pacman -S --noconfirm networkmanager
+        sudo systemctl enable --now NetworkManager;;
+    1)
+        :;;
+    2)
+        sudo systemctl enable --now NetworkManager;;
+esac
 
-if pacman -Qi paru > /dev/null 2>&1; then
-    epic_print_function_blue "paru already installed, skipping."
-else
-    # Install paru from AUR
-    epic_print_function_green "Installing paru
-    "
-    git clone https://aur.archlinux.org/paru.git
-    cd paru || { echo "Failed to enter 'paru' directory"; exit 1; }
-    makepkg -si --noconfirm
-    cd ..
-    rm -r paru/
-fi
+install_paru
 
 # Define the list of packages
 packages=(
@@ -56,11 +99,11 @@ packages=(
     unzip
 )
 
-epic_print_function_green "Installing packages"
+epic_print_function "Installing packages" "green"
 # Install packages if not already installed
 for package in "${packages[@]}"; do
     if pacman -Qi "$package" > /dev/null 2>&1; then
-        epic_print_function_blue "$package is already installed, skipping."
+        epic_print_function "$package is already installed, skipping." "blue"
     else
         paru -S --noconfirm "$package"
     fi
@@ -72,14 +115,14 @@ if [[ "$(pwd)" == *dotfiles* ]]; then
     rm -f install-packages.sh README.md
     # Use stow to manage dotfiles
     if stow . --dotfiles > /dev/null 2>&1; then
-        epic_print_function_blue 'Dotfiles successfully stowed'
+        epic_print_function 'Dotfiles successfully stowed' "blue"
     else
         rm -r ~/.config/
         stow . --dotfiles
     fi
 fi
 
-if $SHELL != zsh; then
+if [[ "$SHELL" != "/bin/zsh" ]]; then
     chsh -s /bin/zsh
 fi
 
