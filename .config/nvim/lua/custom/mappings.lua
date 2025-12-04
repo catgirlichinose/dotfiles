a91d30e444ee110591d8928cab86fb9d8c472090
catgirlichinose <spigolonluca645@gmail.com>
2025-12-04 09:34:26 +0100

    Removed every mention of adi1090x's rofi config and NvChad (the purge is happening)

--- .config/nvim/lua/custom/mappings.lua
+++
@@ -1,3 +0,0 @@
-vim.api.nvim_set_keymap('n', '<F6>', "<cmd>CompilerOpen<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-F6>', "<cmd>CompilerRedo<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-F7>', "<cmd>CompilerToggleResults<cr>", { noremap = true, silent = true})
